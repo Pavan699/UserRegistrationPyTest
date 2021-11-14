fd81a73ceeb10998a19a94d98fcf5aa62b13231e
Pavan699 <pavannakate03@gmail.com>
2021-11-14 20:41:28 +0530

    [Pavan] Add: Adding Title for User Registration problem

---
+++ InfoVAlidation.py
@@ -0,0 +1,8 @@
+'''
+@Author: Pavan Nakate
+@Date: 2021-11-13 10:22
+@Last Modified by: Pavan Nakate
+@Last Modified time: None
+@Title : User-Registration-Program  
+'''
+print("Welcome to User Registration Program")
