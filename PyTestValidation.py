98c3f12d9dd4ec6e43574a905fbb85b3f738312b
Pavan699 <pavannakate03@gmail.com>
2021-11-15 13:24:51 +0530

    [Pavan] Add: Accessing data from json file and testing using pytest

--- PyTestValidation.py
+++ PyTestValidation.py
@@ -7,120 +7,110 @@
 '''
 from InfoVAlidation import Validation
 import pytest
+import json
 
-def test_validFirstName():
-    """
-    Description: 
-        In this test case when given a valid first name should return true.
-    """ 
+with open('ValidData.json') as f:
+    data = json.load(f)
     
-    validFirstName = ["Pavan","Jay"]
+    def test_validFirstName():
+        """
+        Description: 
+            In this test case when given a valid first name should return true.
+        """ 
     
-    for names in validFirstName:
-        assert Validation.validate_first_name(names) == True
+        for user in data:
+            f_name = user['firstName']
+            assert Validation.validate_first_name(f_name) == True
 
-def test_invalidFirstName():
-    """
-    Description: 
-        In this test case when given a invalid first name should return false.
-    """ 
+    def test_validLastName():
+        """
+        Description: 
+            In this test case when given a valid last name should return true.
+        """ 
 
-    invalidFirstName = ["rahul", "anna", "hari23", "s@minum" ]
+        for user in data:
+            l_name = user['lastName']
+            assert Validation.validate_last_name(l_name) == True
 
-    for names in invalidFirstName:
-        assert Validation.validate_first_name(names) == False
+    def test_validMobileNumber():
+        """
+        Description: 
+            In this test case when given a valid mobile number should return true.
+        """ 
 
-def test_validLastName():
-    """
-    Description: 
-        In this test case when given a valid last name should return true.
-    """ 
+        for user in data:
+            phone_no = user['mobile']
+            assert Validation.validate_mobile(phone_no) == True
 
-    validLastName = ["Nakate", "Mane"]
+    def test_validPassword():
+        """
+        Description: 
+            In this test case when given a valid password should return true.
+        """ 
 
-    for names in validLastName:
-        assert Validation.validate_last_name(names) == True
+        for user in data:
+            passcode = user['password']
+            assert Validation.validate_password(passcode) == True
 
+    def test_validEmail():
+        """
+        Description: 
+            In this test case when given a valid email should return true.
+        """ 
 
-def test_invalidLastName():
-    """
-    Description: 
-        In this test case when given a invalid last name should return false.
-    """ 
-
-    invalidLastName = ["rathod", "Shinde3", "xyz$"]
-
-    for names in invalidLastName:
-        assert Validation.validate_last_name(names) == False
-
-def test_validMobileNumber():
-    """
-    Description: 
-        In this test case when given a valid mobile number should return true.
-    """ 
-
-    validMobileNumber = ["91 9960748875", "91 9966332211"]
-
-    for numbers in validMobileNumber:
-        assert Validation.validate_mobile(numbers) == True
+        for user in data:
+            mail = user['email']
+            assert Validation.validate_email(mail) == True
 
 
-def test_invalidMobileNumber():
-    """
-    Description: 
-        In this test case when given a invalid mobile number should return false.
-    """ 
+with open('InvalidData.json') as in_f:
+    invalid_data = json.load(in_f)
 
-    invalidMobileNumber = ["919452147754", "9874563214", "91-9654789665", "5566889900", "91 9854521"]
+    def test_invalidFirstName():
+        """
+        Description: 
+            In this test case when given a invalid first name should return false.
+        """ 
 
-    for numbers in invalidMobileNumber:
-        assert Validation.validate_mobile(numbers) == False
+        for user in invalid_data:
+            f_name = user['firstName']
+            assert Validation.validate_first_name(f_name) == False
 
-def test_validPassword():
-    """
-    Description: 
-        In this test case when given a valid password should return true.
-    """ 
+    def test_invalidLastName():
+        """
+        Description: 
+            In this test case when given a invalid last name should return false.
+        """ 
 
-    validPassword = ["Pavan@699", "$DadaNam3", "Acde7854$"]
+        for user in invalid_data:
+            l_name = user['lastName']
+            assert Validation.validate_last_name(l_name) == False
 
-    for passwords in validPassword:
-        assert Validation.validate_password(passwords) == True
+    def test_invalidMobileNumber():
+        """
+        Description: 
+            In this test case when given a invalid mobile number should return false.
+        """ 
 
+        for user in invalid_data:
+            phone_no = user['mobile']
+            assert Validation.validate_mobile(phone_no) == False
 
-def test_invalidPassword():
-    """
-    Description: 
-        In this test case when given a invalid password should return false.
-    """
+    def test_invalidPassword():
+        """
+        Description: 
+            In this test case when given a invalid password should return false.
+        """
+        for user in invalid_data:
+            passcode = user['password']
+            assert Validation.validate_password(passcode) == False
 
-    invalidPassword = ["Abcd1234", "Raghu#*", "guddi34", "$akkal56", "RAFTAR67*"]
+    def test_invalidEmail():
+        """
+        Description: 
+            In this test case when given a invalid email should return false.
+        """ 
 
-    for passwords in invalidPassword:
-        assert Validation.validate_password(passwords) == False
-
-def test_validEmail():
-    """
-    Description: 
-        In this test case when given a valid email should return true.
-    """ 
-
-    validEmails = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
-                    "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au"]
-
-    for mails in validEmails:
-        assert Validation.validate_email(mails) == True
-
-
-def test_invalidEmail():
-    """
-    Description: 
-        In this test case when given a invalid email should return false.
-    """ 
-
-    inValidEmails = ["abc","abc@.com.my","abc123@gmail.a","abc123@.com",
-                     "abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com",
-                     "abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com"]
-
-    for mails in inValidEmails:
-        assert Validation.validate_email(mails) == False
+        for user in invalid_data:
+            mail = user['email']
+            assert Validation.validate_email(mail) == False
